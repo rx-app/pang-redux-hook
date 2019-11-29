12aaab09c2c3663e7e757273165f218fa00a433c
rxapp <rxapp@yandex.com>
2019-11-29 18:14:51 +0800

    useReducer的使用

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Example5 from './Example5'
 
+// 6useReducer的使用
 
 ReactDOM.render(<Example5 />, document.getElementById('root'));
